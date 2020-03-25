0fff13a32b44a73c11b314157a3a4c8df6d19844
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-03-25 11:22:43 +0800

    新的赋值方式

--- Program01/Program.cs
+++ Program01/Program.cs
@@ -6,15 +6,25 @@ namespace Program01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p1 = new Person() //新的赋值方式
+            {
+                Name = "叶玲",
+                Age = 20,
+                Sex = false
+
+            };
+            if (p1.Sex)
+            {
+                Console.WriteLine("男");
+            }
         }
         
     }
     public class Person 
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public bool Sex { get; set; }
+        public string Name { get; set; }//姓名
+        public int Age { get; set; }//年龄
+        public bool Sex { get; set; }//性别
 
     }
 

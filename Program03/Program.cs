794cffc7d7a6717e53488798f0ee98e834cb99db
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-03-26 10:57:51 +0800

    路程与票价

---
+++ Program03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t = new Ticket(distance: 550);
+            t.ShowTicket();
+            Console.ReadKey();
+        }
+    }
+    public class Ticket
+    {
+        public  double  Distance { get;}
+
+        public Ticket(double distance)
+        {
+            this.Distance = distance > 0 ? distance:0;
+            this.Price = distance > 0 && distance <= 100 ? distance : distance > 100 && distance <= 200 ? 0.95 * distance : distance > 200 && distance <= 300 ? 0.9 * distance : distance > 300 ? 0.85 * distance : 0;
+
+        }
+
+        
+
+        public double Price { get; }
+        public  void ShowTicket()
+        {
+            Console.WriteLine(Price);
+        }
+    }
+}

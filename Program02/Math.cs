1873643e2ac7f6f7b7e4d770fa95daa3b52ddbb5
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-03-26 10:20:12 +0800

    静态方法调用

---
+++ Program02/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program02
+{
+    class Math
+    {
+        public int vala { get; set; }
+        public static int Getsquare(int x) => x * x;
+        //等同于
+        //public int Getsquare(int x){return x*x };
+    }
+}
---
+++ Program02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Math.Getsquare(x: 3));
+
+        }
+    }
+    public class Person 
+    {
+        public string name { get; set; }
+    }
+}

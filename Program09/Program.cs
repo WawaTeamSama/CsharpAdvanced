16b4a649e92968abc10bcd373290b622e0c359db
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-07 10:00:56 +0800

    写入文件

---
+++ Program09/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Program09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using (FileStream  fsWrite=new FileStream("new text",FileMode.OpenOrCreate ,FileAccess.Write))
+            {
+                var str = "覆盖";
+                byte[] buffer = Encoding.UTF8.GetBytes(str);
+                fsWrite.Write(buffer, 0, buffer.Length);
+            }
+            Console.WriteLine("文件写入成为！");
+        }
+    }
+}

80dfd42d0d07234f127e3df02d28b9b1e086ba18
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-08 10:05:12 +0800

    Person

---
+++ Program11/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program11
+{
+    class Person
+    {
+        //唯一标识
+        public Guid ID { get; set; }
+
+        public string EmployeeNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"(FirstName)(LastName)";
+        public bool Sex { get; set; } = true;
+        public DateTime BirthDay { get; set; } = DateTime.Today;
+        public string PhoneNumber { get; set; }
+
+    }
+}

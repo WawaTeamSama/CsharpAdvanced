57aa15d8d020b97ada87bd06d053aacad362599e
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-09 11:03:04 +0800

    SHA

---
+++ Program12Encyption/Encpytion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Program12Encyption
+{
+    class Encpytion
+    {
+        public static string GetMd5(string str)
+        {
+            var md5 = MD5.Create();
+            var bitArray = Encoding.GetEncoding("UTF-8").GetBytes(str);
+
+            var md5BitArray = md5.ComputeHash(bitArray);
+            var encrypStr = "";
+            for(var i = 0; i < md5BitArray.Length; i++)
+            {
+                encrypStr += md5BitArray[i].ToString("x2");
+            }
+            md5.Clear();
+            return encrypStr;
+        }
+        public static string GetSHA(string str)
+        {
+            var sha = SHA512.Create();
+            var bitArray = Encoding.GetEncoding("UTF-8").GetBytes(str);
+
+            var md5BitArray = sha.ComputeHash(bitArray);
+            var encrypStr = "";
+            for (var i = 0; i < md5BitArray.Length; i++)
+            {
+                encrypStr += md5BitArray[i].ToString("x2");
+            }
+            sha.Clear();
+            return encrypStr;
+        }
+    }
+}
---
+++ Program12Encyption/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program12Encyption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pwd = "k23124";
+            Console.WriteLine("明文:" + pwd);
+            string md5Pwd = Encpytion.GetMd5(pwd);
+            Console.WriteLine("MD5密文:" + md5Pwd);
+            string SHAPwd = Encpytion.GetSHA(pwd);
+            Console.WriteLine("SHA密文" + SHAPwd);
+        }
+    }
+}

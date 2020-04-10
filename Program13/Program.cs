e5ee55a51ca2d630380d9098fa15f7f04036a79a
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-10 11:13:13 +0800

    2

--- Program13/Program.cs
+++ Program13/Program.cs
@@ -8,25 +8,42 @@ namespace Program13
         static void Main(string[] args)
         {
             //创建文件夹
-            Directory.CreateDirectory(@"c:\test");
-            Console.WriteLine("创建文件夹成功！");
+            //Directory.CreateDirectory(@"c:\test");
+            //Console.WriteLine("创建文件夹成功！");
             try {
-                //删除文件夹
-                Directory.Delete(@"c:\test");
-                Console.WriteLine("删除文件夹成功!");
+                ////删除文件夹
+                //Directory.Delete(@"c:\test");
+                //Console.WriteLine("删除文件夹成功!");
 
-                //移动文件夹
-                Directory.Move(@"c:\test", @"c:\tee");
+                ////移动文件夹
+                //Directory.Move(@"c:\test", @"c:\tee");
 
-                //判断某个文件夹是否存在
+                ////判断某个文件夹是否存在
                 if (Directory.Exists(@"c:\test"))
                 {
-                    for(var i = 1; i <= 100; i++)
+                    for (var i = 1; i <= 100; i++)
                     {
                         Directory.CreateDirectory(@"c:\test\" + i);
                     }
                 }
-              }
+                //查询文件夹
+                //string keyPath = "100";
+                //string[] path = Directory.GetDirectories(@"c:\test");
+                //foreach(var p in path)
+                //{
+                //    if (p.Contains(keyPath))
+                //    {
+                //        Console.WriteLine(p);
+                //        break;
+                //    }
+                //}
+
+                string[] name = Directory.GetFiles(@"c:\test");
+                foreach (var p in name)
+                {
+                    Console.WriteLine(p);
+                }
+            }
             catch
             {
                 Console.WriteLine("文件夹不存在");

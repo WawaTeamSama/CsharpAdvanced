f3c58924776ca663124ca26cc79ffe8fdd2baf4b
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-10 11:48:57 +0800

    3

---
+++ Program14/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Program14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.lb1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(391, 61);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(214, 158);
+            this.pb.TabIndex = 1;
+            this.pb.TabStop = false;
+            // 
+            // lb1
+            // 
+            this.lb1.FormattingEnabled = true;
+            this.lb1.ItemHeight = 12;
+            this.lb1.Location = new System.Drawing.Point(54, 31);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(259, 304);
+            this.lb1.TabIndex = 3;
+            this.lb1.Click += new System.EventHandler(this.lb1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.pb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.ListBox lb1;
+    }
+}
+
---
+++ Program14/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Program14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lb1_Click(object sender, EventArgs e)
+        {
+            string[] name = Directory.GetFiles(@"c:\test");
+        
+            pb.Image = Image.FromFile(name[lb1.SelectedIndex]);
+           
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] name = Directory.GetFiles(@"c:\test");
+            ArrayList arr = new ArrayList();
+            foreach (string str in name)
+            {
+                str.Substring(8, str.Length);
+                arr.Add(str);
+            }
+           
+            
+            
+            lb1.DataSource = arr;
+        }
+    }
+}

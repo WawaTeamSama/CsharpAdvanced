3082987373dfe4302257eff8b7ab556b128b7fda
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-15 10:32:23 +0800

    1

--- Program19/Program.cs
+++ Program19/Program.cs
@@ -7,13 +7,28 @@ namespace Program19
     {
         static void Main(string[] args)
         {
-            
-            Program thread1 = new Program();
-            thread1.Method();
-            //创建另一个线程来执行
-            ThreadStart ts = new ThreadStart(thread1.Method);
-            Thread t = new Thread(ts);
+            Console.WriteLine("主线程");
+            Thread t = new Thread(new ThreadStart(ThreadProc));
             t.Start();
+            for(var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("主线程做别的事");
+            }
+            Console.WriteLine("主线程：等待threadproc执行完毕");
+            //阻塞线程
+            t.Join();
+
+            for (var i = 5; i < 10; i++)
+            {
+                Console.WriteLine("主线程做别的事");
+            } 
+
+            //Program thread1 = new Program();
+            //thread1.Method();
+            ////创建另一个线程来执行
+            //ThreadStart ts = new ThreadStart(thread1.Method);
+            //Thread t = new Thread(ts);
+            //t.Start();
         }
         public void Method()
         {
@@ -21,6 +36,14 @@ namespace Program19
             int j = 2;
             int result = i + j;
             Console.WriteLine("线程{0},结果{1}", AppDomain.GetCurrentThreadId().ToString(), result);
+        }
+        private static void ThreadProc()
+        {
+            for(var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+
+            }
         }
     }
 }

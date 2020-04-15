52830c0bb8926c68305026298da471465c02b4b4
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-15 11:16:09 +0800

    1

---
+++ Program20/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program20
+{
+    public partial class Form1 : Form
+    {
+        Thread t;
+        public Form1()
+        {
+            InitializeComponent(); 
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t != null)
+            {
+                t.Abort();
+            }
+            t = new Thread(Second);
+
+            t.IsBackground = true;
+            t.Start();
+          
+        }
+        private void Second()
+        {
+            for(int i = 0; i < 100000; i++)
+            {
+                Thread.Sleep(1000);
+                textBox1.Text = i.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;//取消跨线程错误检测
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //当关闭窗体时，把后台线程关闭
+            if (t != null)
+            {
+                t.Abort();
+            }
+        }
+    }
+}

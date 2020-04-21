037087b08458c8e4181c656fb6e2246e06dac2ea
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-21 10:15:01 +0800

    1

---
+++ Program23GDI/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Program23GDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(745, 32);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(89, 26);
+            this.btnLine.TabIndex = 0;
+            this.btnLine.Text = "直线";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 502);
+            this.Controls.Add(this.btnLine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLine;
+    }
+}
+
---
+++ Program23GDI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Program23GDI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            //绘制直线
+            //创建GDI对象
+            Graphics g = this.CreateGraphics();
+            //创建画笔对象
+            Pen pen = new Pen(Brushes.Orange);
+
+            //建立坐标
+            Point p1 = new Point(30, 50);
+            Point p2 = new Point(30, 300);
+
+            g.DrawLine(pen, p1, p2);
+        }
+    }
+}

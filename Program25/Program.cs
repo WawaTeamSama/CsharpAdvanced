1d5a9ff0df8b0d747d64dee929f6773fa72ec961
Administrator <Administrator@JKY885PU1M1TNAQ>
2020-04-23 12:15:19 +0800

    1

---
+++ Program25/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Program25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //用方法实现：计算1-100之间的质数和
+            //质数只能被1和它本身整除
+            Console.WriteLine("1-100之间的质数和为："+primeNumberSum(100));
+        }
+        public static int primeNumberSum(int range)//算总和
+        {
+            int sum = 1;
+           for(int i = 1; i <= range; i++)
+            {
+                if (IsPrime(i))
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        public static bool IsPrime(int n) //判断是否是质数
+        {
+            for(int i = 2; i <= n/2+1; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program26/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Program26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.用方法来实现：有一个字符串数组：马龙，迈克尔乔丹，雷吉米勒，蒂姆邓肯，科比布莱恩特，请输出最长的字符
+        
+            string[] str = new string[5] { "马龙", "迈克尔乔丹", "雷吉米勒", "蒂姆邓肯", "科比布莱恩特" };
+            Console.WriteLine(GetLongestStr(str));
+
+            //2.用方法来实现：请计算出一个整形数组的平均值 1,3,5,7,93,33,4,4,6,8,10 要求计算结果如果有小数，则显示小数点后两位（四舍五入）
+            int[] nums = new int[11] { 1, 3, 5, 7, 93, 33, 4, 4, 6, 8, 10 };
+            Console.WriteLine(GetAve(nums));
+
+        }
+        public static string GetLongestStr(string[] array)
+        {
+            string longest = "";
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (longest.Length - array[i].Length < 0)
+                {
+                    longest = array[i];
+                }
+            }
+            return longest;
+        }
+        public static double GetAve(int[] array)
+        {
+            int sum = 0;
+            foreach(int i in array)
+            {
+                sum += i;
+            }
+            double ave = (double)(sum / array.Length);
+            double result = Math.Round(ave, 4);
+            return result;
+        }
+       
+        //3.请通过冒泡排序法对整数数组1,3,5,7,90,2,4,6,8,10实现升序排序
+    }
+}
